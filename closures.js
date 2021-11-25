2d65d9b8f7869c5b795ca84eedb6414f23fc7f22
André Kovac <me@andrekovac.com>
2021-11-25 20:45:55 +0100

    Add second case to closures example

--- closures.js
+++ closures.js
@@ -1,17 +1,42 @@
-// --------
-// Closures
-// --------
+// --------------------------
+// Closures (primitive types)
+// --------------------------
 
-function outerFunction(frozenNumber) {
-  function innerFunction(variableNumber) {
+function outerFunction1(frozenNumber) {
+  function innerFunction1(variableNumber) {
     // Question: Which output will be logged?
     console.log(variableNumber + frozenNumber)
   }
-  return innerFunction
+  return innerFunction1
 }
 
 // freeze the number 42
-const myClosure = outerFunction(42) // which ugly number do you want to freeze?
+let myNumber = 42;
 
-// use the convenience food:
-myClosure(4)  // Which output will be logged when calling this function?
+const myClosure1 = outerFunction1(myNumber) // which ugly number do you want to freeze?
+
+// Mutate number
+myNumber = 5;
+
+myClosure1(4) // Which output will be logged when calling this function?
+
+
+// ----------------------------
+// Closures (complext objects)
+// ----------------------------
+
+function outerFunction2(frozenObject) {
+  function innerFunction2(variableObject) {
+    // Question: Which output will be logged?
+    console.log(variableObject.count + frozenObject.count)
+  }
+  return innerFunction2
+}
+
+const input = { count: 3 };
+// freeze object
+const myClosure2 = outerFunction2(input);
+// mutate count
+input.count = 6;
+// call closure
+myClosure2({ count: 5 }); // Which output will be logged when calling this function?
